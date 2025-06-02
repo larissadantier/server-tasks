81f1bb761885b1c05a706a916d2717c9dd03bb21
larissadantier <larissa_dantier@hotmail.com>
2025-06-02 19:05:04 -0300

    chore: first commit with initial configuraitons

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import fastifyPostgres from "@fastify/postgres";
+import Fastify from "fastify";
+
+const fastify = Fastify({
+	logger: true,
+});
+
+fastify.register(fastifyPostgres, {
+	connectionString: "",
+});
+
+fastify.setErrorHandler((err, _, reply) => {
+	reply.code(500).send({ message: err.message });
+});
+
+function init() {
+	fastify.listen(
+		{
+			port: Number(process.env.SERVER_PORT),
+			host: process.env.SERVER_HOST,
+		},
+		(err) => {
+			if (err) {
+				fastify.log.error(err);
+				process.exit(1);
+			}
+
+			fastify.log.info("🔥 Server Running on port 3333");
+		},
+	);
+}
+
+init();
